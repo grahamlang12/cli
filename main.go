a3522aa75521b750c255d7f97a503314022e7cd9
LNCracker <loigaak123@gmail.com>
2026-07-06 11:22:14 +0700

    fix: redirect progress messages to stderr when --json is active or stdout is not a TTY

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"golang.org/x/term"
+)
 
 func main() {
-	fmt.Println("Hello, Bounty Hunter!")
+	isJSON := false
+	for _, arg := range os.Args {
+		if arg == "--json" {
+			isJSON = true
+			break
+		}
+	}
+
+	isTTY := term.IsTerminal(int(os.Stdout.Fd()))
+
+	var progressWriter io.Writer
+	if isJSON || !isTTY {
+		progressWriter = os.Stderr
+	} else {
+		progressWriter = os.Stdout
+	}
+
+	fmt.Fprintln(progressWriter, "Loading...")
+	time.Sleep(100 * time.Millisecond)
+	fmt.Fprintln(progressWriter, "Processing...")
+	time.Sleep(100 * time.Millisecond)
+
+	if isJSON {
+		resp := map[string]string{
+			"message": "Hello, Bounty Hunter!",
+			"status":  "success",
+		}
+		jsonData, err := json.MarshalIndent(resp, "", "  ")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error marshaling JSON: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println(string(jsonData))
+	} else {
+		fmt.Println("Hello, Bounty Hunter!")
+	}
 }
